d17506941e3eeb0f05d91eda9c3b5d4d8c645bf9
Edsel Clarin <edsel.clarin@yahoo.com>
2022-07-08 19:10:55 -0700

    Add project files.

---
+++ TxtCsv/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace TxtCsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioCRLF = new System.Windows.Forms.RadioButton();
+            this.radioSpace = new System.Windows.Forms.RadioButton();
+            this.radioTab = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkCopyToClipboard = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 16);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(367, 270);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 289);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Text";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.groupBox2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(759, 295);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(382, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(374, 289);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output Text";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(3, 16);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(368, 270);
+            this.textBox2.TabIndex = 0;
+            // 
+            // radioCRLF
+            // 
+            this.radioCRLF.AutoSize = true;
+            this.radioCRLF.Location = new System.Drawing.Point(17, 25);
+            this.radioCRLF.Name = "radioCRLF";
+            this.radioCRLF.Size = new System.Drawing.Size(57, 17);
+            this.radioCRLF.TabIndex = 3;
+            this.radioCRLF.TabStop = true;
+            this.radioCRLF.Text = "CR/LF";
+            this.radioCRLF.UseVisualStyleBackColor = true;
+            // 
+            // radioSpace
+            // 
+            this.radioSpace.AutoSize = true;
+            this.radioSpace.Location = new System.Drawing.Point(17, 71);
+            this.radioSpace.Name = "radioSpace";
+            this.radioSpace.Size = new System.Drawing.Size(60, 17);
+            this.radioSpace.TabIndex = 3;
+            this.radioSpace.TabStop = true;
+            this.radioSpace.Text = "SPACE";
+            this.radioSpace.UseVisualStyleBackColor = true;
+            // 
+            // radioTab
+            // 
+            this.radioTab.AutoSize = true;
+            this.radioTab.Location = new System.Drawing.Point(17, 48);
+            this.radioTab.Name = "radioTab";
+            this.radioTab.Size = new System.Drawing.Size(46, 17);
+            this.radioTab.TabIndex = 3;
+            this.radioTab.TabStop = true;
+            this.radioTab.Text = "TAB";
+            this.radioTab.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioCRLF);
+            this.groupBox3.Controls.Add(this.radioSpace);
+            this.groupBox3.Controls.Add(this.radioTab);
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(153, 100);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Parse By";
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(342, 80);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 5;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.49644F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28.50356F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(765, 421);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkCopyToClipboard);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.buttonConvert);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 304);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(759, 114);
+            this.panel1.TabIndex = 3;
+            // 
+            // checkCopyToClipboard
+            // 
+            this.checkCopyToClipboard.AutoSize = true;
+            this.checkCopyToClipboard.Location = new System.Drawing.Point(191, 84);
+            this.checkCopyToClipboard.Name = "checkCopyToClipboard";
+            this.checkCopyToClipboard.Size = new System.Drawing.Size(109, 17);
+            this.checkCopyToClipboard.TabIndex = 6;
+            this.checkCopyToClipboard.Text = "Copy to Clipboard";
+            this.checkCopyToClipboard.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 421);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Text to CSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioCRLF;
+        private System.Windows.Forms.RadioButton radioSpace;
+        private System.Windows.Forms.RadioButton radioTab;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkCopyToClipboard;
+    }
+}
+
---
+++ TxtCsv/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TxtCsv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int i = 0;
+            textBox1.TabIndex = i++;
+            textBox2.TabIndex = i++;
+            radioCRLF.TabIndex = i++;
+            radioTab.TabIndex = i++;
+            radioSpace.TabIndex = i++;
+            checkCopyToClipboard.TabIndex = i++;
+            buttonConvert.TabIndex = i++;
+
+            checkCopyToClipboard.Checked = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckRadioButton(radioCRLF);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            InvokeOnClick(buttonConvert, EventArgs.Empty);
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            Convert(textBox1.Text);
+        }
+
+        private void Convert(string inputStr)
+        {
+            if (!String.IsNullOrEmpty(inputStr))
+            {
+                var separators = new List<char>();
+                if (radioCRLF.Checked)
+                {
+                    separators.Add('\r');
+                    separators.Add('\n');
+                }
+                else if (radioTab.Checked)
+                {
+                    separators.Add('\t');
+                }
+                else
+                {
+                    separators.Add(' ');
+                }
+
+                var items = inputStr.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+                var outputStr = String.Join(",", items);
+
+                textBox2.Text = outputStr;
+
+                if (checkCopyToClipboard.Checked)
+                {
+                    CopyToClipboard(outputStr, TextDataFormat.UnicodeText);
+                }
+            }
+        }
+
+        private void CheckRadioButton(RadioButton btn)
+        {
+            radioCRLF.Checked = 
+            radioTab.Checked = 
+            radioTab.Checked = false;
+
+            btn.Checked = true;
+        }
+
+        #region COPY_TO_CLIPBOARD
+
+        public static void CopyToClipboard(string txt, TextDataFormat format)
+        {
+            var clipboardThread = new Thread(() => SendToClipboard(txt, format));
+            clipboardThread.SetApartmentState(ApartmentState.STA);
+            clipboardThread.IsBackground = true;
+            clipboardThread.Start();
+        }
+
+        private static void SendToClipboard(string txt, TextDataFormat format)
+        {
+            Clipboard.SetText(txt, format);
+        }
+
+        #endregion COPY_TO_CLIPBOARD
+    }
+}
